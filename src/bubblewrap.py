cdcb1f06921290d79bd2341a0a14b8b3a46744c2
Sohel Ahmed <69566363+aloner-pro@users.noreply.github.com>
2022-10-16 13:35:34 +0530

    👌 IMPROVE: flake8 linting on the repo (#8)
    * Did flake8 linting on the repo
    
    * changed variable names

--- src/bubblewrap.py
+++ src/bubblewrap.py
@@ -3,7 +3,8 @@ class Bubbles():
     def __init__(self):
         pass
 
-    def get_rich_bubble(self, txt, bg_color="blue", fg_color="white", rich=True):
+    def get_rich_bubble(self, txt, bg_color="blue",
+                        fg_color="white", rich=True):
         circle_style = f"[{bg_color}]"
         circle_close = f"{circle_style[:1]}/{circle_style[1:]}"
         body_open = f"[{fg_color} on {bg_color}]"
@@ -24,19 +25,25 @@ def cli() -> None:
     c.print(b.get_rich_bubble("I love bubblewrap", bg_color="red"), end=" ")
     c.print(b.get_rich_bubble("try it now", bg_color="blue"))
     print()
-    c.print(b.get_rich_bubble("Bubble up words", bg_color="dark_green", fg_color="grey66"), end=" ")
+    c.print(b.get_rich_bubble("Bubble up words",
+            bg_color="dark_green", fg_color="grey66"), end=" ")
     c.print(b.get_rich_bubble("or entire sentences", bg_color="blue"), end=" ")
     c.print(b.get_rich_bubble("try it now", bg_color="purple"))
     print()
-    c.print(b.get_rich_bubble("Use Rich, or ANSI colors of your choosing", bg_color="purple"))
+    c.print(b.get_rich_bubble("Use Rich, or ANSI colors of your choosing",
+            bg_color="purple"))
     print()
     c.print(b.get_rich_bubble("Fully control", bg_color="blue"), end=" ")
     c.print(b.get_rich_bubble("background", bg_color="dark_green"), end=" ")
     c.print(b.get_rich_bubble("and", bg_color="purple"), end=" ")
-    c.print(b.get_rich_bubble("foreground", bg_color="blue", fg_color="dark_red"), end=" ")
-    c.print(b.get_rich_bubble("colors", bg_color="grey66", fg_color="grey3"))
+    c.print(b.get_rich_bubble("foreground", bg_color="blue",
+            fg_color="dark_red"), end=" ")
+    c.print(b.get_rich_bubble("colors", bg_color="grey66",
+            fg_color="grey3"))
     print()
-    c.print(b.get_rich_bubble("Create beautiful CLI applications, with ease!", bg_color="deep_pink4"))
+    c.print(b.get_rich_bubble("Create beautiful CLI applications, with ease!",
+            bg_color="deep_pink4"))
     print()
 
-#print(b.get_ansi_bubbles("Ayo how are you", "\033[31m", "\033[32;41m", "\033[0m"))
+# print(b.get_ansi_bubbles("Ayo how are you",
+#       "\033[31m", "\033[32;41m", "\033[0m"))
