f11352dde869f36fbd1d42026f5e17887a28f93d
Andrzej Szulc <56353699+aaaandrzej@users.noreply.github.com>
2022-10-21 11:50:41 +0200

    🧹 CLEAN UP (#15)
    * Minor code cleanup
    
    * Black formatting

--- src/bubblewrap.py
+++ src/bubblewrap.py
@@ -1,23 +1,26 @@
 class Bubbles:
-
     def __init__(self):
         pass
 
-    def get_rich_bubble(self, txt, bg_color="blue", fg_color="white", rich=True):
+    @staticmethod
+    def get_rich_bubble(txt, bg_color="blue", fg_color="white"):
         circle_style = f"[{bg_color}]"
         circle_close = f"{circle_style[:1]}/{circle_style[1:]}"
         body_open = f"[{fg_color} on {bg_color}]"
         body_close = f"{body_open[:1]}/{body_open[1:]}"
         return f"{circle_style}{circle_close}{body_open}{txt}{body_close}{circle_style}{circle_close}"
 
-    def get_ansi_bubbles(self, txt, circle_style, txt_style, reset):
+    @staticmethod
+    def get_ansi_bubbles(txt, circle_style, txt_style, reset):
         bubble = f"{circle_style}{reset}{txt_style}{txt}{reset}{circle_style}{reset}"
         return bubble
 
-    def get_rich_chain(self, txt, bg_color="blue", fg_color="white", divider=""):
+    @staticmethod
+    def get_rich_chain(txt, bg_color="blue", fg_color="white", divider=""):
         return Link(txt, bg_color, fg_color, divider)
 
-    def get_ansi_chain(self, txt, txt_style, reset, divider=""):
+    @staticmethod
+    def get_ansi_chain(txt, txt_style, reset, divider=""):
         return ANSILink(txt, txt_style, reset, divider)
 
 
@@ -38,7 +41,9 @@ class Link:
 
             # divider fg = prev link bg
             # divider bg = current link bg
-            divider_open, divider_close = get_tags(f"{prev_link.bg_color} on {bg_color}")
+            divider_open, divider_close = get_tags(
+                f"{prev_link.bg_color} on {bg_color}"
+            )
 
             pre_txt += f"{divider_open}{divider}{divider_close}"
 
@@ -107,17 +112,30 @@ def cli() -> None:
     c.print(b.get_rich_bubble("I love bubblewrap", bg_color="red"), end=" ")
     c.print(b.get_rich_bubble("try it now", bg_color="blue"))
     print()
-    c.print(b.get_rich_bubble("Bubble up words", bg_color="dark_green", fg_color="grey66"), end=" ")
+    c.print(
+        b.get_rich_bubble("Bubble up words", bg_color="dark_green", fg_color="grey66"),
+        end=" ",
+    )
     c.print(b.get_rich_bubble("or entire sentences", bg_color="blue"), end=" ")
     c.print(b.get_rich_bubble("try it now", bg_color="purple"))
     print()
-    c.print(b.get_rich_bubble("Use Rich, or ANSI colors of your choosing", bg_color="purple"))
+    c.print(
+        b.get_rich_bubble(
+            "Use Rich, or ANSI colors of your choosing", bg_color="purple"
+        )
+    )
     print()
     c.print(b.get_rich_bubble("Fully control", bg_color="blue"), end=" ")
     c.print(b.get_rich_bubble("background", bg_color="dark_green"), end=" ")
     c.print(b.get_rich_bubble("and", bg_color="purple"), end=" ")
-    c.print(b.get_rich_bubble("foreground", bg_color="blue", fg_color="dark_red"), end=" ")
+    c.print(
+        b.get_rich_bubble("foreground", bg_color="blue", fg_color="dark_red"), end=" "
+    )
     c.print(b.get_rich_bubble("colors", bg_color="grey66", fg_color="grey3"))
     print()
-    c.print(b.get_rich_bubble("Create beautiful CLI applications, with ease!", bg_color="deep_pink4"))
+    c.print(
+        b.get_rich_bubble(
+            "Create beautiful CLI applications, with ease!", bg_color="deep_pink4"
+        )
+    )
     print()
